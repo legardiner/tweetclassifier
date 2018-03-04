0800e5823c1e0ea6f726cc7e9c00eec30a157a41
legardiner <legardiner@mail.lipscomb.edu>
2018-03-04 11:33:10 -0600

    Add feature to right user input and results to RDS

--- app/flaskapp.py
+++ app/flaskapp.py
@@ -3,6 +3,10 @@ import os, sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append('..')
 from develop.src.models import predictModel
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv, find_dotenv
+
 
 app = Flask(__name__)
 
@@ -13,8 +17,30 @@ def my_form():
 
 @app.route("/", methods=['POST'])
 def my_form_post():
+	# Get input from form
 	tweet = request.form['tweet']
+	# Predict tweet classification with user inputed tweet
 	obama_prob, trump_prob, classification = predictModel.classify_tweet(tweet)
+
+	# Store results in dataframe to load into database
+	df = pd.DataFrame({'tweet': tweet, 'obama_prob': [obama_prob], 'trump_prob': [trump_prob], 'classification': classification})
+
+	# Find .env automagically by walking up directories until it's found
+	dotenv_path = find_dotenv()
+
+	# Load up the entries as environment variables
+	load_dotenv(dotenv_path)
+
+	# Get connection string from environment variables
+	connection_string = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+	# Create sqlalchemy database connection
+	conn = create_engine(connection_string)
+
+	# Append user input and results to database
+	df.to_sql('user_tweets', conn, index=False, if_exists='append')
+
+	# Return html page with results
 	return render_template('classifier_results.html', classification=classification, obama_prob=obama_prob, trump_prob=trump_prob)
 
 if __name__ == "__main__":
